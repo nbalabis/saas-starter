6fd2aae09b1bca2bae8ce2105a6e3f09d6e0df21
nbalabis <88114907+nbalabis@users.noreply.github.com>
2023-07-20 17:32:08 -1000

    Rename files

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,8 +6,8 @@ import { ClerkProvider } from "@clerk/nextjs";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Genie",
-  description: "Your personal AI Genie",
+  title: "Saas Starter",
+  description: "My Saas starter",
 };
 
 export default function RootLayout({
