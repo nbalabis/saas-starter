9ec8de5ece9bf895373f79428553906db65edc99
nbalabis <88114907+nbalabis@users.noreply.github.com>
2023-07-20 17:28:57 -1000

    Set up Clerk authentication

--- app/(dashboard)/(routes)/dashboard/page.tsx
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -1,5 +1,12 @@
+import { UserButton } from "@clerk/nextjs";
+
 const DashboardPage = () => {
-  return <div>Dashboard Page (Protected)</div>;
+  return (
+    <div>
+      <p>Dashboard Page (Protected)</p>
+      <UserButton afterSignOutUrl="/" />
+    </div>
+  );
 };
 
 export default DashboardPage;
--- app/(landing)/page.tsx
+++ app/(landing)/page.tsx
@@ -1,5 +1,20 @@
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+
 const LandingPage = () => {
-  return <div>Landing Page (Unprotected)</div>;
+  return (
+    <div>
+      Landing Page (Unprotected)
+      <div>
+        <Link href="sign-in">
+          <Button>Log In</Button>
+        </Link>
+        <Link href="sign-up">
+          <Button>Sign Up</Button>
+        </Link>
+      </div>
+    </div>
+  );
 };
 
 export default LandingPage;
