a91e201aa93ec89f445670d28af7f003471d0f34
nbalabis <88114907+nbalabis@users.noreply.github.com>
2023-07-26 13:15:52 -1000

    Create a layout for dashboard pages

---
+++ app/(dashboard)/layout.tsx
@@ -0,0 +1,18 @@
+import Sidebar from "@/components/navigation/sidebar";
+import Navbar from "@/components/navigation/navbar";
+
+const DashboardLayout = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <div className="relative h-full">
+      <div className="z-[80] hidden h-full md:fixed md:inset-y-0 md:flex md:w-72 md:flex-col">
+        <Sidebar />
+      </div>
+      <main className="md:pl-72">
+        <Navbar />
+        {children}
+      </main>
+    </div>
+  );
+};
+
+export default DashboardLayout;
